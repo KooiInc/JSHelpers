d0766562c722a05b62a86171779efeff88cdedec
Renzo Kooi <renzo.kooi@gmail.com>
2014-12-21 12:00:18 +0100

    Update Helpers.js
    Added isObj, useJQ

--- Helpers.js
+++ Helpers.js
@@ -1,6 +1,8 @@
 var Helpers = (function() {
   return {
-    report: reportHTML
+    report: reportHTML,
+    isObj: isPlainObject,
+    useJQ: loadJQ
   }
 
   function reportHTML() {
@@ -12,6 +14,28 @@ var Helpers = (function() {
           return r;
       }();
     report.innerHTML += '<p>' + [].slice.call(arguments).join() + '</p>';
+  }
+  
+  function isPlainObject(item) {
+    return (
+        item &&
+        typeof item === "object" &&
+        Object.prototype.toString.call(item) === "object Object" &&
+        !item.nodeType
+    );
+  }
+
+  function loadJQ(cb) {
+      if (w.jQuery) {
+        return function () {return void( cb && cb());}
+      }
+      return function () {
+        var head  = d.querySelector('head')
+           ,jq    = d.createElement('script');
+        void( cb && jq.addEventListener('load',cb) );
+        jq.src    = '//code.jquery.com/jquery-2.1.1.min.js';
+        return head.appendChild(jq);
+      };
   }
 
 }());
