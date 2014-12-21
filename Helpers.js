55c690f5e71db777a3b9a11fddf1b5ba9d9a72e7
Renzo Kooi <renzo.kooi@gmail.com>
2014-12-21 12:09:18 +0100

    Update Helpers.js
    Add extensions

--- Helpers.js
+++ Helpers.js
@@ -2,8 +2,9 @@ var Helpers = (function() {
   return {
     report: reportHTML,
     isObj: isPlainObject,
-    useJQ: loadJQ
-  }
+    useJQ: loadJQ,
+    augment: extensions
+  };
 
   function reportHTML() {
     var report = document.querySelector('#result') ||
@@ -38,4 +39,183 @@ var Helpers = (function() {
       };
   }
 
+   // a few usefull augments/polyfills
+  function extensions() {
+        // remove double values from an array  
+        function noDoubles(arr) {
+          arr = arr || this;
+          return arr.filter(function(val) {
+           return !this[val] ? ((this[val] = true), true) : false;
+          }, {});
+        }
+  
+        function args2Array(args){
+          var arr = [];
+          for (var i=0;i<args.length;i+=1) {
+            arr.push(args[i]);
+          }
+          return arr;
+        }
+  
+        function stringformat() {
+                var args = args2Array(arguments);
+                return this.replace(/(\{\d+\})/g, function(a){
+                    return args[+(a.substr(1,a.length-2))||0];
+                });
+        };
+  
+        // determine value frequencies in an array
+        function frequencies(arr) {
+          var mapped = {sum: 0};
+          (arr || this).map(function (a){
+              if (!(a in this)) { this[a] = 1; }
+              else { this[a] += 1; }
+              this.sum += 1;
+              return a; }, mapped
+          );
+          return mapped;
+        }
+  
+        String.Format = function(){
+            var args = [].slice.call(arguments);
+            return stringformat.apply(args[0],args.slice(1));
+        };
+  
+        String.prototype.format = function () {
+          return stringformat.apply(this,arguments);
+        }
+  
+        String.prototype.repeat = function(n){
+            var s = this, r = '';
+            while(n--) {
+                r += s;
+            }
+            return r;
+        };
+  
+        // run functions sequentially
+        Function.prototype.andThen = function () {
+         var args = [].slice.call(arguments)
+            ,next = args.slice(1);
+         this();
+         return args[0] && next.length
+                 ? args[0].andThen.apply(args[0], next)
+                 : args.length == 1  ? args[0]()
+                 : true;
+        };
+  
+        // is character @ [atpos] upperCase?
+        boolean:String.prototype.charIsUpper = function (atpos){
+          var chr = this.charAt(atpos);
+          return /[A-Z]|[\u0080-\u024F]/.test(chr) && chr === chr.toUpperCase();
+        };
+  
+        String.prototype.firstUp = function () {
+          return this.slice(0,1).toUpperCase()+this.slice(1).toLowerCase();
+        }
+  
+        String.prototype.isValidEmail = function() {
+          // should be sufficient
+          return  /^[\w._-]{1,}[+]?[\w._-]{0,}@[\w.-]+\.[a-zA-Z]{2,6}$/.test(this);
+        }
+  
+        Array.prototype.toRE = function (){
+          try { return RegExp.apply(null, this); }
+          catch(e) { return /.*/; }
+        }
+  
+        Array.toRE = function(arr) {
+          return ([].toRE.call(arr));
+        }
+  
+        String.prototype.reCleanup = function(encodeHTML){
+          var str = encodeHTML ? this.replace(/[\u0080-\u024F]/g, function(a) {return '&#'+a.charCodeAt(0)+';';}) : this;
+          return str.replace(/[?*|.+$\/]|\\/g, function(c) {return c==='\\' ? '' : '\\\\'+c;});
+        }
+  
+        String.concat = function(joinchar){
+          joinchar = joinchar || '';
+          return [].slice.call(arguments).join(joinchar);
+        }
+  
+        // MDN Array filter polyfill
+        if (!Array.prototype.filter) {
+          Array.prototype.filter = function(fun)
+          {
+            "use strict";
+  
+            if (this === void 0 || this === null)
+              throw new TypeError();
+  
+            var t = Object(this);
+            var len = t.length >>> 0;
+            if (typeof fun != "function")
+              throw new TypeError();
+  
+            var res = [];
+            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++)
+            {
+              if (i in t)
+              {
+                var val = t[i];
+  
+                // NOTE: Technically this should Object.defineProperty at
+                //       the next index, as push can be affected by
+                //       properties on Object.prototype and Array.prototype.
+                //       But that method's new, and collisions should be
+                //       rare, so use the more-compatible alternative.
+                if (fun.call(thisArg, val, i, t))
+                  res.push(val);
+              }
+            }
+  
+            return res;
+          };
+        }
+  
+        // MDN Array map polyfill
+        if (!Array.prototype.map) {
+          Array.prototype.map = function(fun)
+          {
+            "use strict";
+  
+            if (this === void 0 || this === null)
+              throw new TypeError();
+  
+            var t = Object(this);
+            var len = t.length >>> 0;
+            if (typeof fun !== "function")
+              throw new TypeError();
+  
+            var res = new Array(len);
+            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++)
+            {
+              // NOTE: Absolute correctness would demand Object.defineProperty
+              //       be used.  But this method is fairly new, and failure is
+              //       possible only if Object.prototype or Array.prototype
+              //       has a property |i| (very unlikely), so use a less-correct
+              //       but more portable alternative.
+              if (i in t)
+                res[i] = fun.call(thisArg, t[i], i, t);
+            }
+  
+            return res;
+        };
+       }
+       Array.prototype.uniquify = noDoubles;
+       Array.prototype.frequencies = Array.prototype.frequencies || frequencies;
+  
+       Array.prototype.each = Array.prototype.each || function (fn,rewrite) {
+       for (var i = 0; i < this.length; i++) {
+        if (rewrite){
+          this[i] = fn(this[i]);
+        } else {
+         fn(this[i]);
+        }
+       }
+       return this;
+     };
+  }
 }());
