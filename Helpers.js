2a6cf1372cc3cde3d6e45676bb4247567d683922
Renzo Kooi <renzo.kooi@gmail.com>
2014-12-21 12:25:30 +0100

    Update Helpers.js

--- Helpers.js
+++ Helpers.js
@@ -1,3 +1,4 @@
+ "use strict";
 var Helpers = (function() {
   return {
     report: reportHTML,
@@ -142,8 +143,6 @@ var Helpers = (function() {
         if (!Array.prototype.filter) {
           Array.prototype.filter = function(fun)
           {
-            "use strict";
-  
             if (this === void 0 || this === null)
               throw new TypeError();
   
@@ -154,17 +153,9 @@ var Helpers = (function() {
   
             var res = [];
             var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-            for (var i = 0; i < len; i++)
-            {
-              if (i in t)
-              {
+            for (var i = 0; i < len; i++) {
+              if (i in t)  {
                 var val = t[i];
-  
-                // NOTE: Technically this should Object.defineProperty at
-                //       the next index, as push can be affected by
-                //       properties on Object.prototype and Array.prototype.
-                //       But that method's new, and collisions should be
-                //       rare, so use the more-compatible alternative.
                 if (fun.call(thisArg, val, i, t))
                   res.push(val);
               }
@@ -178,8 +169,6 @@ var Helpers = (function() {
         if (!Array.prototype.map) {
           Array.prototype.map = function(fun)
           {
-            "use strict";
-  
             if (this === void 0 || this === null)
               throw new TypeError();
   
@@ -190,13 +179,7 @@ var Helpers = (function() {
   
             var res = new Array(len);
             var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-            for (var i = 0; i < len; i++)
-            {
-              // NOTE: Absolute correctness would demand Object.defineProperty
-              //       be used.  But this method is fairly new, and failure is
-              //       possible only if Object.prototype or Array.prototype
-              //       has a property |i| (very unlikely), so use a less-correct
-              //       but more portable alternative.
+            for (var i = 0; i < len; i++) {
               if (i in t)
                 res[i] = fun.call(thisArg, t[i], i, t);
             }
